233e4e5e2f60fb462aa544e92de4fdb6bc9c8f41
arvind <37983251+arv01@users.noreply.github.com>
2024-06-26 17:32:57 +0530

    update

--- src/app/Common/Footer/Footer.tsx
+++ src/app/Common/Footer/Footer.tsx
@@ -41,32 +41,32 @@ const Footer = () => {
                     <ul>
                     <li>
                             <a href="https://www.facebook.com/astroshreelive" target="_blank" rel="noopener noreferrer">
-                                <Image width={40} src={facebook.src} alt="astroshree-Facebook" />
+                                <Image  src={facebook.src} width={40} height={40} alt="astroshree-Facebook" />
                             </a>
                         </li>
                         <li>
                             <a href="https://x.com/astroshreeapp" target="_blank" rel="noopener noreferrer">
-                                <Image width={40} src={twitter.src} alt="astroshree-Twitter" />
+                                <Image  src={twitter.src} width={40} height={40} alt="astroshree-Twitter" />
                             </a>
                         </li>
                         <li>
                             <a href="https://www.instagram.com/astroshriapp" target="_blank" rel="noopener noreferrer">
-                                <Image width={40} src={insta.src} alt="astroshree-Instagram" />
+                                <Image  src={insta.src} width={40} height={40} alt="astroshree-Instagram" />
                             </a>
                         </li>
                         <li>
                             <a href="https://www.linkedin.com/in/astro-shree-1058a6315/" target="_blank" rel="noopener noreferrer">
-                                <Image width={40} src={linkedin.src} alt="astroshree-LinkedIn" />
+                                <Image src={linkedin.src}  width={40} height={40} alt="astroshree-LinkedIn" />
                             </a>
                         </li>
                         <li>
                             <a href="https://www.youtube.com/channel/UCgEsMgrs7FKO7NnFXCGQH-w" target="_blank" rel="noopener noreferrer">
-                                <Image width={40} src={youtube.src} alt="astroshree-YouTube" />
+                                <Image  src={youtube.src} width={40} height={40} alt="astroshree-YouTube" />
                             </a>
                         </li>
                         <li>
                             <a href="https://www.pinterest.com/astroshreelive/" target="_blank" rel="noopener noreferrer">
-                                <Image width={40} src={pinterest_145808.src} alt="astroshree-Pinterest" />
+                                <Image  src={pinterest_145808.src} width={40} height={40} alt="astroshree-Pinterest" />
                             </a>
                         </li>
                     </ul>
